96d4920aa159e1f48f641e0a644f5843b227a259
Kogo <115625940+Ivykogo@users.noreply.github.com>
2024-11-16 20:00:45 +0300

    Create functions.py
    done with week 3 assignment

---
+++ functions.py
@@ -0,0 +1,9 @@
+def calculate_discount(price, discount_percent):
+    if discount_percent >= 20:
+        return price * (1 - discount_percent/100)
+    else:
+        return price
+price = float(input("Enter the original price: "))
+discount_percent = float(input("Enter the discount percentage: "))   
+final_price = calculate_discount(price, discount_percent) 
+print(f"The final price after the discount is: {final_price}")
